fd1927c5291ac970c23eca71cb1423489a4c9175
Elod23 <vargaelod23@gmail.com>
2022-11-07 17:17:26 +0200

    add project structure

--- src/DemoTara.sol
+++ src/DemoTara.sol
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import @openzeppelin/contracts@4.7.3/token/ERC20/ERC20.sol;
-import @openzeppelin/contracts@4.7.3/token/ERC20/extensions/ERC20Burnable.sol;
-import @openzeppelin/contracts@4.7.3/access/Ownable.sol;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DemoTara is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20(DemoTara, DMT) {}
+    constructor() ERC20("DemoTara", "DMT") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 }
-
